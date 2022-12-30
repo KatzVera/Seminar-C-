929364457a732ce119a49ea96dfc100a94f0b9fc
Katz Vera <Katz.VG@gmail.com>
2022-12-30 19:20:56 +0300

    Add task 003

---
+++ Task003/Program.cs
@@ -0,0 +1,7 @@
+﻿// Вывод ответа на 5 разных строк последовательно из (число/nчисло/n...)
+
+Console.Clear();
+Console.Write("Введите число: ");
+int n = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("1\n2\n3\n4\n5");
+
