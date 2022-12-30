43354d690ca38be22c5a8720043ac8100e8119ff
Katz Vera <Katz.VG@gmail.com>
2022-12-30 19:23:05 +0300

    Add task 006_2

---
+++ Task006_2/Program.cs
@@ -0,0 +1,13 @@
+﻿// Задача 5. Решение через цикл while
+
+Console.Clear();
+Console.Write("Введите число: ");
+int n = Convert.ToInt32(Console.ReadLine());
+int i = (-1) * n;
+while (i <= n)
+{
+    Console.Write($"{i} ");
+    i++; // i = i + 1
+}
+
+
