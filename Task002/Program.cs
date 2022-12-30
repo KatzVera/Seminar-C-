b196b01f4213315d4baaeebde483dd709b168927
Katz Vera <Katz.VG@gmail.com>
2022-12-30 19:20:01 +0300

    Add task 002

---
+++ Task002/Program.cs
@@ -0,0 +1,8 @@
+﻿// Задачка на вывод квадрата числа
+
+Console.Clear();
+Console.Write("Введите число: ");
+int n = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine($"Результат: {n * n}"); //Интерполяция строк
+
+
