fd17d4a6c044c626bb0c9945f79722ff1f044b55
Katz Vera <Katz.VG@gmail.com>
2022-12-30 19:21:16 +0300

    Add task 004

---
+++ Task004/Program.cs
@@ -0,0 +1,13 @@
+﻿// Задание 1: проверка 1е число квадрат 2го?
+
+Console.Clear();
+Console.Write("Введите число 1-e: ");
+int n = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите число 2-e: ");
+int m = Convert.ToInt32(Console.ReadLine());
+if (m * m == n)
+    Console.WriteLine("yes");
+else
+    Console.WriteLine("no");
+
+
