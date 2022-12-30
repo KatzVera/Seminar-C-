47e8b3906bdf2f92e3f355db1f5d4f5a1286dffa
Katz Vera <Katz.VG@gmail.com>
2022-12-30 19:23:25 +0300

    Add task 007

---
+++ Task007/Program.cs
@@ -0,0 +1,8 @@
+﻿// Задача 7: вывести последную цифру введенного числа
+
+Console.Clear();
+Console.Write("Введите число: ");
+int n = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine(n % 10);
+
+
