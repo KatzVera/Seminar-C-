2aaa3c4bb1a2b2fdcd911646f82a0def2e5fde67
Katz Vera <Katz.VG@gmail.com>
2022-12-30 19:19:26 +0300

    Add task 001

---
+++ Task001/Program.cs
@@ -0,0 +1,16 @@
+﻿// Про високосный год
+
+Console.Clear();
+Console.Write("Введите год: ");
+int year = Convert.ToInt32(Console.ReadLine()); // |2 147 000 000|
+
+// int a = int.Parse(Console.ReadLine());
+// || - или (or, +)
+// && - и (and, *)
+
+if (year % 400 == 0 || year % 100 != 0 && year % 4 == 0)
+    Console.WriteLine("yes");
+else
+    Console.WriteLine("no");
+
+
