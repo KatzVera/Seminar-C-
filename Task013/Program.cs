147bc4bcc2f16c125e6e466e119cedd714416bca
Katz Vera <Katz.VG@gmail.com>
2023-01-05 20:31:46 +0300

    add 013

---
+++ Task013/Program.cs
@@ -0,0 +1,12 @@
+﻿// Задача 16
+
+Console.Clear();
+Console.Write("введите число 1: ");
+int n = Convert.ToInt32(Console.ReadLine());
+Console.Write("введите число 2: ");
+int m = Convert.ToInt32(Console.ReadLine());
+if (n * n == m || m * m == n)
+    Console.WriteLine("да");
+else
+    Console.WriteLine("нет");
+    
