c04ed696cff79b4cd160882b1884e59abf3de4b9
Katz Vera <Katz.VG@gmail.com>
2023-01-05 20:30:39 +0300

    add 011

---
+++ Task011/Program.cs
@@ -0,0 +1,13 @@
+﻿//Задача 12
+
+Console.Clear();
+Console.Write("введите число 1: ");
+int n = Convert.ToInt32(Console.ReadLine());
+Console.Write("введите число 2: ");
+int m = Convert.ToInt32(Console.ReadLine());
+
+if (n % m == 0)
+    Console.WriteLine("кратно");
+else
+    Console.WriteLine($"не кратно, остаток {n % m}");
+
