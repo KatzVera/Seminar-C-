ff75aa6eda5b8596fec6bcdc3d8180c41511ee5b
Katz Vera <Katz.VG@gmail.com>
2023-01-05 20:31:19 +0300

    add 012

---
+++ Task012/Program.cs
@@ -0,0 +1,10 @@
+﻿// Задача 14
+
+Console.Clear();
+Console.Write("введите число: ");
+int n = Convert.ToInt32(Console.ReadLine());
+if (n % 7 == 0 && n % 23 == 0)
+    Console.WriteLine("да");
+else
+    Console.WriteLine("нет");
+    
