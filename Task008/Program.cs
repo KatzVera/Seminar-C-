6cea61a8904f5a34d4f675b7bd0f45e682e00a58
Katz Vera <Katz.VG@gmail.com>
2023-01-05 20:28:48 +0300

    add 008

---
+++ Task008/Program.cs
@@ -0,0 +1,13 @@
+﻿// dop
+
+Console.Clear();
+
+int number = new Random().Next(10, 100); // [10, 99]
+Console.WriteLine($"Сгенерированное число: {number}");
+int n1 = number / 10;
+int n2 = number % 10;
+if (n1 > n2)
+    Console.WriteLine(n1);
+else
+    Console.WriteLine(n2);
+
