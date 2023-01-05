7057ecae8f86201d4c5a044ae0b6b88569faf9c2
Katz Vera <Katz.VG@gmail.com>
2023-01-05 20:30:10 +0300

    add 010

---
+++ Task010/Program.cs
@@ -0,0 +1,11 @@
+﻿// задача 11
+
+Console.Clear();
+
+int number = new Random().Next(100, 1000); // [100, 999)
+Console.WriteLine($"Сгенерированное число: {number}");
+int n1 = number / 100;
+int n3 = number % 10;
+Console.WriteLine(n1 * 10 + n3 *1);
+// ConsolWrite(n1); при таком выводе получается 
+//ConsoleWrite(n3);     2 отдельных числа, а не целое двухзначное
