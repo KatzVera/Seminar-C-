8ee9c85eb2aa2944fd66ecab3f817cca6f8437d1
Katz Vera <Katz.VG@gmail.com>
2023-01-05 20:32:19 +0300

    add 014

---
+++ Task014/Program.cs
@@ -0,0 +1,10 @@
+﻿// ДЗ доп. задача
+
+Console.Clear();
+Console.Write("");
+int n = Convert.ToInt32(Console.ReadLine());
+while (n != 0)
+{
+    n = Convert.ToInt32(Console.ReadLine());
+}
+Console.WriteLine("end");
