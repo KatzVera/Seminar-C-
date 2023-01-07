b9795ee0c77fef3189b933e5f235bd381579dd9f
Katz Vera <Katz.VG@gmail.com>
2023-01-07 16:30:50 +0300

    task 17

---
+++ Task017/Program.cs
@@ -0,0 +1,13 @@
+﻿// Задача 21
+
+Console.Clear();
+Console.Write("Введите координату X 1-й точки: ");
+double x_1 = Convert.ToDouble(Console.ReadLine());
+Console.Write("Введите координату Y 1-й точки: ");
+double y_1 = Convert.ToDouble(Console.ReadLine());
+Console.Write("Введите координату X 2-й точки: ");
+double x_2 = Convert.ToDouble(Console.ReadLine());
+Console.Write("Введите координату Y 2-й точки: ");
+double y_2 = Convert.ToDouble(Console.ReadLine());
+
+Console.WriteLine(Math.Round(Math.Sqrt(Math.Pow(x_1 - x_2, 2) + Math.Pow(y_1 - y_2, 2)), 2));
